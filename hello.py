1bb5a08d7cd03c1ab8c6e8b92114a1eaa68ed1a1
Eduard Khiaev <ekhiaev@ISRAKIOSK.Local>
2020-01-13 15:17:56 +0200

    simple form

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    if request.method == 'POST' and 'name' in request.form:
+        name = request.form['name']
+    return render_template('index.html', name=name)
 
 
 @app.route('/user/<name>')
