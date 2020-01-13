e2811a6178d27644ac97f44d5bd169a457bc8582
Eduard Khiaev <ekhiaev@ISRAKIOSK.Local>
2020-01-13 17:34:42 +0200

    WTForms

--- hello.py
+++ hello.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Length
+import os
+
+SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 bootstrap = Bootstrap(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[
+                       Required(), Length(1, 16)])
+    submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
-    if request.method == 'POST' and 'name' in request.form:
-        name = request.form['name']
-    return render_template('index.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
