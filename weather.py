c586ee5e51b764afe593d135ffc60cbe89e9587d
Eduard Khiaev <ekhiaev@ISRAKIOSK.Local>
2020-01-13 13:40:57 +0200

    custom 404 page

--- weather.py
+++ weather.py
@@ -25,6 +25,9 @@ def index():
     }
     return render_template('weather.html', city='Tel-aviv', months=months, weather=weather)
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
