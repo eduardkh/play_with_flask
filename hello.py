c3a68c87ae3b54872403e4bad50db773529fe19e
Eduard Khiaev <ekhiaev@ISRAKIOSK.Local>
2020-01-13 11:04:06 +0200

    custom hello

--- hello.py
+++ hello.py
@@ -7,5 +7,10 @@ def index():
     return 'Hello World!'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello {name}!</h1>'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
